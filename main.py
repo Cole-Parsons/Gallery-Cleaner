a266fac0ce2b0b4e7f078cd759bb2f8cf88392b1
Surfs-Up5 <cgparsons5@gmail.com>
2026-04-04 00:32:04 -0400

    now parses through users selected folder and takes only images, (not tested)

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
+from pathlib import Path
 
-def get_File_Path():
-    file_path = filedialog.askdirectory()
-    print(file_path)
+selected_folder = None
+
+def get_file_path():
+    global selected_folder
+    selected_folder = filedialog.askdirectory()
+    print('Selected folder: ', selected_folder)
+    if selected_folder:
+        parse_images_folder()
+
+def parse_images_folder():
+    image_files = [f for f in selected_folder.iterdir() if f.suffix.lower() in [".png", ".jpg", ".jpeg", "heic", "heif"]]
+    print(f"Found {len(image_files)} images")
+
+#add displaying image to window
 
 
+# GUI
 window = Tk()
-button = Button(text='Open', command=get_File_Path)
+button = Button(text='Open', command=get_file_path)
 button.pack()
 
 image = Image.open('C:/Users/cgpar/Desktop/pictures/download.jpg')
@@ -16,7 +29,5 @@ image = ImageTk.PhotoImage(image)
 
 my_label = Label(window, image=image)
 my_label.pack(pady=20)
-
-
 
 window.mainloop()
