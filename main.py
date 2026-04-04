27b9bf276b9cde0550ea4d3b7bfe2342b957d19a
Surfs-Up5 <cgparsons5@gmail.com>
2026-04-04 00:04:22 -0400

    ui opens up a image from a hard coded path

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from tkinter import *
 from tkinter import filedialog
+from PIL import ImageTk, Image
 
-def openFile():
+def get_File_Path():
     file_path = filedialog.askdirectory()
     print(file_path)
 
 
 window = Tk()
-button = Button(text='Open', command=openFile)
+button = Button(text='Open', command=get_File_Path)
 button.pack()
+
+image = Image.open('C:/Users/cgpar/Desktop/pictures/download.jpg')
+image = ImageTk.PhotoImage(image)
+
+my_label = Label(window, image=image)
+my_label.pack(pady=20)
+
+
+
 window.mainloop()
